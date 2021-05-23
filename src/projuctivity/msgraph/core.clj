e574dbc688b78fcaa831a00a40b876fecc07138b
Pierre Larochelle <pierrebombay@gmail.com>
2021-05-23 14:32:12 -0700

    Adding basic request functionality

---
+++ src/projuctivity/msgraph/core.clj
@@ -0,0 +1,36 @@
+(ns projuctivity.msgraph.core
+  (:require [clj-http.client :as http]
+            [cheshire.core :as json]
+            [projuctivity.msgraph.auth :as auth]))
+
+(defn load-edn
+  "Load edn from an io/reader source (filename or io/resource)."
+  [source]
+  (read-string (slurp source)))
+
+(def scopes ["user.read"
+             "Tasks.ReadWrite"
+             "Calendars.ReadWrite"])
+
+(def config (assoc (load-edn ".config.edn")
+                   :scopes scopes))
+
+(def base-url "https://graph.microsoft.com/")
+
+(defn get
+  ([resource params token]
+   (try
+     (let [url (format "%s/v1.0/%s" base-url resource)
+           params {:headers {:authorization (format "Bearer %s" token)}
+                   :query-params params}
+           resp (http/get url params)]
+       (json/parse-string (:body resp)))
+     (catch Exception e
+       (if (= (:status (ex-data e)) 401)
+         (do
+           (auth/refresh-token config)
+           (get resource params))
+         (throw e)))))
+  ([resource params]
+   (let [token (auth/token config)]
+     (get resource params token))))
