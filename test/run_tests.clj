f28b6fa0d71a9b71be9aec644572209dc9ab6f00
Pierre Larochelle <pierrebombay@gmail.com>
2021-06-19 19:29:50 -0700

    Returning correct exit code

--- test/run_tests.clj
+++ test/run_tests.clj
@@ -3,7 +3,13 @@
              [projuctivity.msgraph.core-test :as core-test]
              [projuctivity.msgraph.auth.pure-test :as auth-pure-test]))
 
-(defn -main [& args]
-  (t/run-tests 'projuctivity.msgraph.core-test
-               'projuctivity.msgraph.auth.pure-test))
+(defn passed? [result]
+  (every? zero?
+          (map result [:fail :error])))
 
+(defn -main [& args]
+  (let [res (t/run-tests 'projuctivity.msgraph.core-test
+                         'projuctivity.msgraph.auth.pure-test)]
+    (System/exit (if (passed? res)
+                   0
+                   1))))
