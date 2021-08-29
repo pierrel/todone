1cf38e89ffba1d94772f8b73590ff7901899e08c
Pierre Larochelle <pierrebombay@gmail.com>
2021-08-29 19:03:46 +0000

    Adding dev ns

---
+++ test/dev.clj
@@ -0,0 +1,9 @@
+(ns dev
+  (:require [projuctivity.core :as proj]
+            [clojure.spec.test.alpha :as stest]))
+
+(stest/instrument)
+
+(comment
+  (proj/events-between "2021-01-01" "2021-02-01"))
+
