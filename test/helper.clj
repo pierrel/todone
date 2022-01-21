1455948c4f88822d02ea567dbdee17c346bd8ac6
Pierre Larochelle <pierre@larochelle.io>
2022-01-21 21:46:21 +0000

    Adding new helper function to better stub and check functions.

--- test/helper.clj
+++ test/helper.clj
@@ -1,17 +1,29 @@
 (ns helper
   (:require  [clojure.test :as t]
              [clojure.pprint :as pprint]
+             [clojure.set :as setop]
              [clojure.spec.test.alpha :as spectest]))
 
-(defn check-and-report-specs [namespace & {:keys [to-stub]}]
-  (let [stub-lookup (zipmap to-stub (repeat true))]
-    (doseq [fun (spectest/enumerate-namespace namespace)]
-      (if (get stub-lookup fun)
-        (spectest/instrument fun {:stub #{fun}})
-        (spectest/instrument fun))
-      (let [result (spectest/check fun)
-            summary (spectest/summarize-results result)]
-        (pprint/write summary :stream nil)
-        (t/is (or (zero? (:total summary))
-                  (= (:total summary)
-                     (:check-passed summary))))))))
+(defn test-summary
+  ([summary & summaries]
+   (doseq [sum (conj summaries summary)]
+     (test-summary sum)))
+  ([summary]
+   (t/is (or (zero? (:total summary))
+             (= (:total summary)
+                (:check-passed summary))))))
+
+(defn check-and-report-specs
+  "Checks all of `namespace` without `to-stub` and returns
+  a summary of each individual spec check."
+  [namespace & {:keys [to-stub]}]
+  (let [all-fns (spectest/enumerate-namespace namespace)
+        to-check (setop/difference all-fns to-stub)]
+    (spectest/instrument to-check)
+    (doseq [stub to-stub]
+      (spectest/instrument stub {:stub #{stub}}))
+    (map (comp spectest/summarize-results spectest/check)
+         to-check)))
+
+(def check-and-test-specs (comp (partial apply test-summary)
+                                check-and-report-specs))
--- test/projuctivity/msgraph/auth/urls_test.clj
+++ test/projuctivity/msgraph/auth/urls_test.clj
@@ -1,6 +1,7 @@
 (ns projuctivity.msgraph.auth.urls-test
   (:require [clojure.test :as t]
+            [projuctivity.msgraph.auth.urls :as suite]
             [helper :as helper]))
 
 (t/deftest spec-checks
-  (helper/check-and-report-specs 'projuctivity.msgraph.auth.urls))
+  (helper/check-and-test-specs 'projuctivity.msgraph.auth.urls))
